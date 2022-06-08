8a6a331404f0eb0c05815919b3f2085aa4db5ad6
tamilselvanps <mailtotamil16@gmail.com>
2022-06-08 16:30:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateTodo from './components/CreateTodo'
+import Todos from './components/Todos'
+
+export default class App extends React.Component {
+
+  state = {
+    todos : []
+  }
+
+  onAddItem =(todoObj)=>{
+    this.setState({todos:[...this.state.todos,todoObj]})
+  }
+
+  onRemoveItem =(index)=>{
+    const currItems = this.state.todos.filter((toDo)=>{
+        return index !== toDo.id
+    })
+    this.setState({todos:currItems})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <CreateTodo onTodoSubmit={this.onAddItem} todos={this.state.todos}/>
+        <Todos tasksList={this.state.todos} onRemoveItem={this.onRemoveItem}/>
+      </div>
+    )
+  }
 }
 
-export default App;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -7,22 +7,23 @@ export default class Todos extends React.Component{
     render(){
         if(!this.props.tasksList.length){
             return <div>You don't have any pending tasks</div>
+        }else{
+            const todosList = this.props.tasksList
+            return(
+                <>
+                    <div className={styles.todoWrapper}>Your Todos</div>
+                    <div>
+                    {todosList.map((toDo) => (
+                        <Todo
+                            index={toDo.id}
+                            task={toDo.task}
+                            isCompleted={toDo.isCompleted}
+                            onRemove={this.props.onRemoveItem}
+                        />
+                    ))}
+                    </div>
+                </>
+            )
         }
-        const todosList = this.props.tasksList
-        return(
-            <>
-                <div className={styles.todoWrapper}>Your Todos</div>
-                <div>
-                {todosList.map((toDo) => (
-                    <Todo
-                        index={toDo.id}
-                        task={toDo.task}
-                        isCompleted={toDo.isCompleted}
-                        onRemove={this.props.onRemoveItem}
-                    />
-                ))}
-                </div>
-            </>
-        )
     }
 }
